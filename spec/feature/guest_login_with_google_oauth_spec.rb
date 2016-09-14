ebef6a81c889d381e300793b30d1091229d794e6
Lane Winham <mlwinham12@gmail.com>
2016-09-14 17:48:20 -0600

    Add oauth for google, test passing

--- spec/feature/guest_login_with_google_oauth_spec.rb
+++ spec/feature/guest_login_with_google_oauth_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.feature 'Guest login with google oauth' do
   scenario 'while on welcome page guest clicks login with google' do
+    setup_for_oauth
+
     visit '/'
 
     expect(page).to have_content("Welcome to the best app you never knew you needed!")
     expect(page).to have_content("Would you like to login with")
-    expect(page).to have_link("Google")
+    expect(page).to have_button("Google")
 
-    click_on "Google"
+    click_button "Google"
 
     expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Bobby")
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,3 +69,25 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+def setup_for_oauth
+  Capybara.app = RandomRes::Application
+  stub_omniauth
+end
+
+def stub_omniauth
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    "uid"=>"1234",
+    "info"=>
+    {"name"=>"Bobby Robby",
+      "email"=>"BR@example.com",
+      "first_name"=>"Bobby",
+      "last_name"=>"Robby",
+      "image"=>"volcano.jpg"},
+      "credentials"=>{"token"=>"yepthisthegoods"},
+      "extra"=>
+      {"id_token"=>
+        "letsgoooo"}})
+end
+# ya29.CjBeA2AdNMt9jLnymJEtlX_7-yy5HUckrQ93ZnAJSNxUxM_nf-aF0h4zCPFosH5LyNI
