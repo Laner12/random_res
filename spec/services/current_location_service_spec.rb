7807df393f29e1ee87b6afe1bab2bc0bf44ff91a
Lane Winham <mlwinham12@gmail.com>
2016-09-15 12:29:37 -0600

    Alter test for vcr compliance

--- spec/services/current_location_service_spec.rb
+++ spec/services/current_location_service_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "CurrentLocationService", type: :model do
-  VCR.use_cassette("current_location") do
     it "should return data for lat and lng" do
+      VCR.use_cassette("current_location") do
       current_location = CurrentLocationService.new.get_location
 
       expect(current_location).to be_kind_of(Hash)
