7bb19fde800ee8003d98b4a54d8d5dce2b4345ba
Lane Winham <mlwinham12@gmail.com>
2016-09-17 13:57:50 -0600

    Test passing for yelp search service

--- app/services/yelp_search_service.rb
+++ app/services/yelp_search_service.rb
@@ -6,7 +6,7 @@ class YelpSearchService
   def api_call(latitude_longitude)
     consumer = OAuth::Consumer.new( ENV['CONSUMER_KEY'],ENV['CONSUMER_SECRET'], {:site => "http://api.yelp.com", :signature_method => "HMAC-SHA1", :scheme => :query_string})
     access_token = OAuth::AccessToken.new( consumer, ENV['TOKEN'],ENV['TOKEN_SECRET'])
-    response = access_token.get("/v2/search?ll=#{latitude_longitude}&radius_filter=3218")
+    response = access_token.get("/v2/search?ll=#{latitude_longitude}&limit=10")
     parse(response.body)["businesses"]
   end
 
