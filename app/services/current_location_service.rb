b75085237b380853f9387e606025f944bbb410d1
Lane Winham <mlwinham12@gmail.com>
2016-09-17 19:12:06 -0600

    Add first round of styling

--- app/services/current_location_service.rb
+++ app/services/current_location_service.rb
@@ -2,7 +2,6 @@ class CurrentLocationService
 
   def initialize
     @_connection = Faraday.post("https://www.googleapis.com/geolocation/v1/geolocate?key=#{ENV['GOOGLE_API_KEY']}")
-    # can probably refactor the call above using key: ENV vs "key" => ENV
   end
 
   def get_location
