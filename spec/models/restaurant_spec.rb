afdd0b0d1214c4107e8fda08aab48493f88df8eb
Lane Winham <mlwinham12@gmail.com>
2016-09-19 12:34:37 -0600

    Add mild styling to search page

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Restaurant" do
     presenter = Restaurant.all_by_location("39.8027644,-105.08748419999999").first
 
     expect(presenter.name).to eq("The Arvada Tavern")
-    expect(presenter.rating_img_url_large).to eq("https://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png")
+    expect(presenter.rating_img_url).to eq("https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png")
     expect(presenter.review_count).to eq(356)
     expect(presenter.image_url).to eq("https://s3-media3.fl.yelpcdn.com/bphoto/WqEbyDOMSGHMVZmBuS_lXA/ms.jpg")
     expect(presenter.url).to eq("https://www.yelp.com/biz/the-arvada-tavern-arvada?adjust_creative=GDuUHYsncVCRnXKpBt8GIw&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=GDuUHYsncVCRnXKpBt8GIw")
