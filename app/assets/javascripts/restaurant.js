46e5a1443249a9a25a09502a2f29511af3199c4c
Lane Winham <mlwinham12@gmail.com>
2016-09-21 17:25:56 -0600

    Add test for sad path and psuedo code for js

---
+++ app/assets/javascripts/restaurant.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  // record the first jumbotron
+  // render the first restaurant (sub 0?)
+  // add information to the data being presented
+  // remove the first restaurant
+  // present the second one
+})
